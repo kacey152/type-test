6132589b3e86916b8d0e77b7e7f799b62177e9ba
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-08-25 11:36:15 +0800

    added progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './ProgressBar.css'
+import React from "react";
+import "./ProgressBar.css";
 
 export const ProgressBar = () => {
   return (
-    <div className='track border border-black m-5'>
-        <div className='character'></div>
+    <div className="track border border-black m-5">
+      <div className="character"></div>
+      <div
+        className="progress"
+        role="progressbar"
+        aria-label="Animated striped example"
+        aria-valuenow="75"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        <div
+          className="progress-bar progress-bar-striped progress-bar-animated"
+          style={{width: "75%"}}
+        ></div>
+      </div>
     </div>
-  )
-}
+  );
+};
