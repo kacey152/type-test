93a2d3d027ef4d4c7973c57c495097d791ff3138
Kacey Chua <110165967+kacey152@users.noreply.github.com>
2023-11-23 20:58:28 +0800

    editted base url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001', // Adjust the URL accordingly
+    baseURL: 'https://type-test-backend.onrender.com', // Adjust the URL accordingly
 });
 export const getLeaderboards = () => {
     return axiosInstance.get('/api/leaderboards');
